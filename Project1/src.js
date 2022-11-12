993d50672db4543e109dce73b95c77b8678bc2f8
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-11-13 01:11:09 +0530

    UI improve & Changes in Some Project

--- Project1/src.js
+++ Project1/src.js
@@ -1,8 +1,8 @@
 function onlyNumbers(evt) {
-    var e = evt; 
+    var e = evt;
     var charCode = e.keyCode;
-    if(charCode>41 &&(charCode<48 || charCode>57))
-    return false;
+    if (charCode > 41 && (charCode < 48 || charCode > 57))
+        return false;
     return true;
 }
 
@@ -16,14 +16,13 @@ function NumToWord(inputNumber, outputControl) {
 
     numLength = rev.length;
     var word = new Array();
-    var j=0;
-    for(i=0; i<numLength; i++) {
+    var j = 0;
+    for (i = 0; i < numLength; i++) {
 
-        switch(i)
-        {
+        switch (i) {
             case 0:
-                if((rev[i]==0)|| (rev[i+1]==1)) {
-                    word[j]= "";
+                if ((rev[i] == 0) || (rev[i + 1] == 1)) {
+                    word[j] = "";
                 }
                 else {
                     word[j] = once[rev[i]];
@@ -31,19 +30,19 @@ function NumToWord(inputNumber, outputControl) {
                 word[j] = " " + word[j];
                 break;
 
-             case 1:
-                 aboveTens();
-                 break;
+            case 1:
+                aboveTens();
+                break;
 
             case 2:
-                if(rev[i] == 0) {
-                    word[j]="";
+                if (rev[i] == 0) {
+                    word[j] = "";
                 }
-                else if((rev[i-1]==0)||(rev[i-1]==0)) {
-                    word[j]=once[rev[i]] + " Hundred";
+                else if ((rev[i - 1] == 0) || (rev[i - 1] == 0)) {
+                    word[j] = once[rev[i]] + " Hundred";
                 }
                 else {
-                    word[j]=once[rev[i]] + " Hundred and ";
+                    word[j] = once[rev[i]] + " Hundred and ";
                 }
                 break;
             case 3:
@@ -57,13 +56,13 @@ function NumToWord(inputNumber, outputControl) {
         j++;
     }
     function aboveTens() {
-        if(rev[i]==0) {word[j]="";}
-        else if(rev[i]==1) {word[j]=twos[rev[i-1]];}
+        if (rev[i] == 0) { word[j] = ""; }
+        else if (rev[i] == 1) { word[j] = twos[rev[i - 1]]; }
         else { word[j] = tens[rev[i]]; }
     }
     word.reverse();
-    var finalOutput=" ";
-    for(i=0; i<numLength; i++) {
+    var finalOutput = " ";
+    for (i = 0; i < numLength; i++) {
         finalOutput = finalOutput + word[i];
     }
     document.getElementById(outputControl).innerHTML = finalOutput;
